78647e7d84a298729e94887b0860096563444766
rass225 <31581061+rass225@users.noreply.github.com>
2018-04-20 22:55:32 +0300

    Changes

--- src/P1_BooleanExpressions.java
+++ src/P1_BooleanExpressions.java
@@ -7,30 +7,28 @@
 /*
  Your expression predictions:
  
- 1.
- 2.
- 3. 
- 4.
- 
- 5.
- 6.
- 7.
- 8.
+ 1. True
+ 2. True
+ 3. False
+ 4. True
+ 5. True
+ 6. False
+ 7. True
+ 8. True
    
  */
 
 /*
  Were you correct? Explain if you were not (what did you learn?)
  
- 1.
- 2.
- 3. 
- 4.
- 
- 5.
- 6.
- 7.
- 8.
+ 1. Correct
+ 2. Correct
+ 3. Correct
+ 4. Correct
+ 5. Correct
+ 6. Correct
+ 7. Correct
+ 8. Correct
 
  */
 public class P1_BooleanExpressions {
@@ -41,7 +39,15 @@ public class P1_BooleanExpressions {
 		int z = 32; 
 		boolean b = false; 
 		
-		System.out.println("REPLACE THIS STRING WITH YOUR BOOLEAN EXPRESSION");
+		System.out.println(!b);
+		System.out.println(b || true );
+		System.out.println((x > y) && (y > z) );
+		System.out.println((x == y) || (x <= z));
+		System.out.println(!(x % 2 == 0));
+		System.out.println(b && !b );
+		System.out.println(b || !b);
+		System.out.println((x < y) == b);
+		
 	}
 
 }
--- src/P3_Consecutive.java
+++ src/P3_Consecutive.java
@@ -23,10 +23,23 @@ public class P3_Consecutive {
 			int num3 = console.nextInt();
 		
 			System.out.printf( "a: %d, b: %d, c: %d -> ", num1, num2, num3);
-			
-
-			
-			System.out.print("Would you like to do it again (enter 'n' to quit)? ");
+			if((num2==num1-1) && (num3== num2-1)) {
+				System.out.print("These are consecuative numbers!");
+			} else if ((num2==num1+1) && (num3==num2+1)) {
+				System.out.print("These are consecuative numbers!");
+			} else if ((num2==num1+2) &&( num3==num1+1)) {
+				System.out.print("These are consecuative numbers!");
+			} else if ((num2==num1-1)&& (num3==num1+1)) {
+				System.out.print("These are consecuative numbers!");
+			} else if ((num2==num1-2) && (num3==num1-1)) {
+				System.out.print("These are consecuative numbers!");
+			} else if ((num2==num1+1) && (num3==num1-1)){
+				System.out.print("These are consecuative numbers!");
+			} else {
+				System.out.print("These are not consecuative numbers!");
+			}
+			System.out.println();
+			System.out.println("Would you like to do it again (enter 'n' to quit)? ");
 			doItAgainResponse = console.next();
 		} while( !doItAgainResponse.equalsIgnoreCase("n"));
 		
